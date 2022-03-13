d4509b2ecd5163ef2ef0a326f8c7f3000e20f866
Rob Becker <robrbecker@gmail.com>
2022-03-13 17:51:24 -0600

    another try

--- tool/auto_release_on_merge.dart
+++ tool/auto_release_on_merge.dart
@@ -48,7 +48,8 @@ void main(List<String> args) async {
   print('New Version    : $newVersion');
 
   var rn = await gh.repositories.generateReleaseNotes(CreateReleaseNotes(
-      slug.owner, slug.name, newVersion));
+      slug.owner, slug.name, newVersion,
+      previousTagName: currentVersion));
 
   var releaseNotes = rn.body.replaceFirst('## What\'s Changed','');
   releaseNotes = '## $newVersion\n$releaseNotes';
