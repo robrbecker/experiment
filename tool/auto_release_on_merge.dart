913088355ec9999bdfe8a0a2fec168d947515f09
Rob Becker <robrbecker@gmail.com>
2022-03-13 21:49:59 -0600

    test push

--- tool/auto_release_on_merge.dart
+++ tool/auto_release_on_merge.dart
@@ -10,6 +10,11 @@ const semvers = ['major', 'minor', 'patch'];
 /// [pr number] PR number of which to release
 /// the semver label is expected to be on the PR
 void main(List<String> args) async {
+  run('echo hi > generated.txt');
+  run('git add .');
+  run('git commit -m gen');
+  run('git push');
+  return;
   if (args.length < 2) {
     print('Usage: dart tool/auto_release_on_merge owner_and_repo pull_number');
     exit(1);
